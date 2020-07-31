ffe1d4d71cc37349618104ba7b22c2401ab4fddb
imxande <alerodriguezlarosa@gmail.com>
2020-07-31 11:53:51 -0400

    Home component implemented, and renders info to the screen

---
+++ portfolio/src/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ale_image from "./images/ale_profile.jpeg";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="image">
+        <span>
+          <img
+            src={ale_image}
+            alt="Alejandro E. Rodriguez, sitting at Liberty University computer lab"
+          />
+        </span>
+      </div>
+      <div className="greeting">
+        <h3>Hi friends!</h3>
+        <h1>I'm Alejandro E. Rodriguez La Rosa</h1>
+        <p>
+          I am a Full Stack Web Developer. I live in North Carolina. As a
+          developer I create responsive, accessible and functional websites and
+          applications. I am curious about how everything works. Always willing
+          to relocate.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
