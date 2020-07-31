bf0d6f9ab50cc1d8d73da5426d9a0929cdcade14
imxande <alerodriguezlarosa@gmail.com>
2020-07-31 18:17:33 -0400

    set up a route for the Home component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
