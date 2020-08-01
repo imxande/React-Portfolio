051bff26c13ea2ede79cb11657bb08284267e703
imxande <alerodriguezlarosa@gmail.com>
2020-07-31 20:04:17 -0400

    added about me component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home";
 import { Switch, Route } from "react-router-dom";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
       </Switch>
     </div>
   );
