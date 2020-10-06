e8806c2cfa691fa29bbef6fcb9c16e6704415fd0
imxande <alerodriguezlarosa@gmail.com>
2020-10-06 16:03:51 -0400

    working on particles background

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,30 +1,36 @@
 import React from "react";
 import ale_image from "../images/ale_profile.jpeg";
 import NavBar from "./NavBar";
+import Particle from "./Particle";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="hero-image">
-        <span>
-          <img
-            src={ale_image}
-            alt="Alejandro E. Rodriguez, sitting at Liberty University computer lab"
-          />
-        </span>
+    <div>
+      <div className="particles">
+        <Particle />
       </div>
-      <div className="greeting">
-        <h3>Hi friends!</h3>
-        <h1>I'm Alejandro E. Rodriguez La Rosa</h1>
-        <p>
-          I am a Full Stack Web Developer. I live in North Carolina and I am
-          willing to relocate. As a developer I create responsive, accessible
-          functional websites and applications. I have always been curious about
-          how everything works.
-        </p>
-      </div>
-      <div className="navigation">
-        <NavBar />
+      <div className="home">
+        <div className="hero-image">
+          <span>
+            <img
+              src={ale_image}
+              alt="Alejandro E. Rodriguez, sitting at Liberty University computer lab"
+            />
+          </span>
+        </div>
+        <div className="greeting">
+          <h3>Hi friends!</h3>
+          <h1>I'm Alejandro E. Rodriguez La Rosa</h1>
+          <p>
+            I am a Full Stack Web Developer. I live in North Carolina and I am
+            willing to relocate. As a developer I create responsive, accessible
+            functional websites and applications. I have always been curious
+            about how everything works.
+          </p>
+        </div>
+        <div className="navigation">
+          <NavBar />
+        </div>
       </div>
     </div>
   );
