682ee71176d6c326b20931ad489f692642ec5ef5
imxande <alerodriguezlarosa@gmail.com>
2020-10-14 21:22:19 -0400

    added header to Projects container

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/Home";
 import { Switch, Route } from "react-router-dom";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
       </Switch>
     </div>
   );
---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./NavBar";
+
+const Projects = () => {
+  return (
+    <div className="projects">
+      <div className="top-container">
+        <h2>PROJECTS</h2>
+        <span>MY WORK</span>
+      </div>
+      <div className="navigation">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
