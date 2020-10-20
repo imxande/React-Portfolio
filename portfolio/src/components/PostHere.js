785ace3807423592623ca4363fac7ba3a7873a85
imxande <alerodriguezlarosa@gmail.com>
2020-10-19 21:38:27 -0400

    added modal class to postHere

--- portfolio/src/components/PostHere.js
+++ portfolio/src/components/PostHere.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "../css/Connect.css";
 
 const PostHere = () => {
+  // close functionality
+  const handleClose = (event) => {
+    event.preventDefault();
+    if (event.target.className === "close") {
+      console.log(event.className);
+    }
+  };
   return (
-    <div>
-      <h1>Hello from PostHere</h1>
+    <div className="modal">
+      <div className="modal-content">
+        <span
+          onClick={handleClose}
+          className="close"
+          style={{ display: "block" }}
+        >
+          &times;
+        </span>
+        <h1>Hello from PostHere</h1>
+      </div>
     </div>
   );
 };
