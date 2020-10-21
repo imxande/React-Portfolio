0962a3100350cfbbd5899a2e57655fd0224461ad
imxande <alerodriguezlarosa@gmail.com>
2020-10-21 04:39:41 -0400

    contact page added

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./NavBar";
+import "../css/Contact.css";
 
 const Contact = () => {
   return (
@@ -7,6 +8,31 @@ const Contact = () => {
       <div className="top-container">
         <h2>CONTACT</h2>
         <span>CONNECT</span>
+      </div>
+      <div className="hire-me">
+        <form
+          className="form-container"
+          method="POST"
+          action="https://formspree.io/alerodriguezlarosa@gmail.com"
+        >
+          <div className="form-input">
+            <label for="name">Name</label>
+            <input type="name" name="name" id="name" size="50" />
+          </div>
+          <div className="form-input">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" size="50" />
+          </div>
+          <div className="form-input">
+            <label for="message">Message</label>
+            <textarea name="message" id="message" rows="6" cols="53"></textarea>
+          </div>
+          <div className="form-button">
+            <button className="btn" type="submit">
+              Send Message
+            </button>
+          </div>
+        </form>
       </div>
       <div className="navigation">
         <Navbar />
