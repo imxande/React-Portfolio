18398873c0b5d5d5cccdabb13a4325f046ebb753
imxande <alerodriguezlarosa@gmail.com>
2020-10-22 23:19:53 -0400

    added breakpoints for mobile and tablet

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -5,41 +5,69 @@ import { IconContext } from "react-icons";
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <ul>
-        <li className="icon-box">
-          <IconContext.Provider value={{ className: "icons" }}>
-            <FaHome />
-          </IconContext.Provider>
-          <Link to="/">
-            <h2 className="navText">Home</h2>
-          </Link>
-        </li>
-        <li className="icon-box">
-          <IconContext.Provider value={{ className: "icons" }}>
-            <FaUser />
-          </IconContext.Provider>
-          <Link to="/about">
-            <h2 className="navText">About</h2>
-          </Link>
-        </li>
-        <li className="icon-box">
-          <IconContext.Provider value={{ className: "icons" }}>
-            <FaToolbox />
-          </IconContext.Provider>
-          <Link to="/projects">
-            <h2 className="navText">Projects</h2>
-          </Link>
-        </li>
-        <li className="icon-box">
-          <IconContext.Provider value={{ className: "icons" }}>
-            <FaEnvelopeOpen />
-          </IconContext.Provider>
-          <Link to="/contact">
-            <h2 className="navText">Contact</h2>
-          </Link>
-        </li>
-      </ul>
+    <div>
+      <div className="navbar">
+        <ul>
+          <li className="icon-box">
+            <IconContext.Provider value={{ className: "icons" }}>
+              <FaHome />
+            </IconContext.Provider>
+            <Link to="/">
+              <h2 className="navText">Home</h2>
+            </Link>
+          </li>
+          <li className="icon-box">
+            <IconContext.Provider value={{ className: "icons" }}>
+              <FaUser />
+            </IconContext.Provider>
+            <Link to="/about">
+              <h2 className="navText">About</h2>
+            </Link>
+          </li>
+          <li className="icon-box">
+            <IconContext.Provider value={{ className: "icons" }}>
+              <FaToolbox />
+            </IconContext.Provider>
+            <Link to="/projects">
+              <h2 className="navText">Projects</h2>
+            </Link>
+          </li>
+          <li className="icon-box">
+            <IconContext.Provider value={{ className: "icons" }}>
+              <FaEnvelopeOpen />
+            </IconContext.Provider>
+            <Link to="/contact">
+              <h2 className="navText">Contact</h2>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="menu-wrap">
+        <input type="checkbox" className="toggler" />
+        <div className="hamburger">
+          <div></div>
+        </div>
+        <div className="menu">
+          <div>
+            <div>
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
